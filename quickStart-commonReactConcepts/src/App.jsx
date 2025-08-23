84c64940e1aac21eb12e8ec8bd33d2fedbb716f9
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2025-08-22 20:52:08 -0500

    Writing markup with JSX

--- quickStart-commonReactConcepts/src/App.jsx
+++ quickStart-commonReactConcepts/src/App.jsx
@@ -1,17 +1,28 @@
-
 //Creating a react component
-function MyButton(){
-  return (
-    <button>I'm a button</button>
-  );
+function MyButton() {
+	return <button>I'm a button</button>;
 }
 
-//Will nest the MyButton Component into App Component which is our major component in this case
-export default function App(){
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
-  );
+//Writing markup with JSX
+function AboutPage() {
+	return (
+		<>
+			<h1>About</h1>
+			<p>
+				Hello there.
+				<br />
+				How do you do?
+			</p>
+		</>
+	);
+}
+
+//Will nest the MyButton Component into App Component which is our main component in this file
+export default function App() {
+	return (
+		<div>
+			<h1>Welcome to my app</h1>
+			<MyButton />
+		</div>
+	);
 }
