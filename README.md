b4bf273e6263d17a2b8be255710540f1dbc7e71a
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2025-08-22 21:45:21 -0500

    Explored adding styles and displaying data.

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # react19.1-playground
 A personal practice and learning space for exploring concepts from the React 19.1 documentation. This repository contains structured examples, demos, and exercises covering new and existing features in React, including components, hooks, state management, and concurrent rendering.
+Install nodeJs
+Install npm
+Scaffold React project with Vite
+
+Delete:
+All content of App.css
+All content of App.jsx
+Comment out index.css import in main.jsx
 Quick Start
 -Create and nest components
 -Add markup and styles
@@ -7,3 +15,14 @@ Quick Start
 -Render conditions and lists
 -Respond to events and update the screen
 -Share data between components
+
+Adding Styles
+-Use this within AboutPage component
+-Implement the CSS in App.css
+-Import App.css into App.jsx
+
+Displaying Data
+-Add your image into src/assets to use within the image tag
+-Create a user object to define user profile details
+-Create a Profile component and use display user details
+-Adapt the styles to the profile using inline CSS within JSX
--- quickStart-commonReactConcepts/src/App.jsx
+++ quickStart-commonReactConcepts/src/App.jsx
@@ -1,13 +1,24 @@
-//Creating a react component
+//import css file
+import "./App.css";
+
+//Create user object
+const user = {
+	name: "Eria Othieno Pinyi",
+	imageUrl: "src/assets/Eria.jpeg",
+	imageSize: 90,
+};
+
+// creating your first react component
 function MyButton() {
 	return <button>I'm a button</button>;
 }
 
-//Writing markup with JSX
+// writing markup with JSX
 function AboutPage() {
 	return (
 		<>
 			<h1>About</h1>
+			<Profile />
 			<p>
 				Hello there.
 				<br />
@@ -17,11 +28,30 @@ function AboutPage() {
 	);
 }
 
+// profile component
+function Profile() {
+	return (
+		<>
+			<h1>{user.name}</h1>
+			<img
+				className="avatar"
+				src={user.imageUrl}
+				alt={"Photo of " + user.name}
+				style={{
+					width: user.imageSize,
+					height: user.imageSize,
+				}}
+			/>
+		</>
+	);
+}
+
 //Will nest the MyButton Component into App Component which is our main component in this file
 export default function App() {
 	return (
 		<div>
 			<h1>Welcome to my app</h1>
+			<AboutPage />
 			<MyButton />
 		</div>
 	);
